b6a15176f14fd41a9e85eb6806c1c82a1e66234d
varundhanda-diligentic <varundhanda.diligentic@gmail.com>
2025-03-10 14:44:24 +0530

    add customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
-"use client"
-import { Scanner } from '@yudiel/react-qr-scanner';
-
+"use client";
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setScanFileId] = useState("No result");
+
+  const handleScan = async (scanData: any) => {
+    console.log(scanData, "scanData");
+    if (scanData) {
+      setScanFileId(scanData[0]?.rawValue);
+      alert(scanData[0]?.rawValue);
+    }
+  };
   return (
     <div className="h-full w-full">
-          <Scanner onScan={(result) => console.log(result)} />
-     
+      <Scanner
+        onScan={handleScan}
+        onError={(error: any) => {
+          alert(error?.message);
+        }}
+        components={{
+          audio: true,
+          onOff: true,
+          torch: true,
+          zoom: true,
+          finder: true,
+        }}
+        // paused={true}
+      />
     </div>
   );
 }
